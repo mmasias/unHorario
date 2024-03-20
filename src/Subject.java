143a822ef013be8739e7968d372511712a891a70
manuel@AIO.universidad <manuel@masiasweb.com>
2024-03-20 18:12:35 +0100

    Normalización y actualización de créditos en Subject
    - Se asegura que el código de cada `Subject` se almacene en mayúsculas.
    - Se añade método `setCredits` para actualizar los créditos de `Subject`.

--- src/Subject.java
+++ src/Subject.java
@@ -8,12 +8,18 @@ public class Subject {
         assert name!=null && code.length()==4 && credits >=0;
 
         this.name = name;
-        this.code = code;
+        this.code = code.toUpperCase();
         this.credits = credits;
     }
 
     public Subject(String name){
-        this(name,name.substring(0, 4).toUpperCase(),0);
+        this(name,name.substring(0, 4),0);
+    }
+
+    public void setCredits(int credits){
+        assert credits>=0;
+        
+        this.credits = credits;
     }
 
     public String show() {
@@ -30,5 +36,10 @@ public class Subject {
         subject = new Subject("Programación 2");
         System.out.println(subject.show());
         System.out.println(subject.showCode());
+
+
+        subject.setCredits(6);
+        System.out.println(subject.show());
+
     }
 }
